229cb25a3038171d6561b525c94b16f04eb79c99
Farzad <farzad.moghaddampoor@certa-systems.com>
2022-10-09 15:23:43 +0200

    Two way binding

--- src/app/Server/server.component.ts
+++ src/app/Server/server.component.ts
@@ -1,8 +1,14 @@
 import {Component} from "@angular/core"
+
 @Component({
-    selector: 'app-server',
-	templateUrl: "./server.component.html"
+  selector: 'app-server',
+  templateUrl: "./server.component.html"
 })
 export class ServerComponent {
-    
+
+  // Server id
+  serverId: number = 10;
+
+  // Server status
+  serverStatus: string = 'Down';
 }
